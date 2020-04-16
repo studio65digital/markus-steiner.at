4b74302f66a4fe47f48abcd668f2256522c3c5d8
Markus Steiner <office@stagedive.media>
2020-04-16 11:24:40 +0200

    add Refernezen

--- gridsome.config.js
+++ gridsome.config.js
@@ -31,6 +31,18 @@ module.exports = {
                               externalLinksRel: ["nofollow", "noopener", "noreferrer"]
                         }
                   }
+            },
+            {
+                  use: "@gridsome/source-filesystem",
+                  options: {
+                        path: "content/Referenzen/**/*.md",
+                        typeName: "Referenzen",
+                        resolveAbsolutePaths: true,
+                        remark: {
+                              externalLinksTarget: "_blank",
+                              externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+                        }
+                  }
             }
       ]
 };
