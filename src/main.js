ca01bf41665527f97afed185590794339331edd7
Markus Steiner <office@stagedive.media>
2020-04-16 14:29:34 +0200

    sd

--- src/main.js
+++ src/main.js
@@ -7,3 +7,10 @@ import DefaultLayout from '~/layouts/Default.vue';
 export default function (Vue) {
       Vue.component('Layout', DefaultLayout);
 }
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// import style
+import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
