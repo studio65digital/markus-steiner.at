d38153295e1b22776905638f6079ec66786e4bf7
Markus Steiner <office@stagedive.media>
2020-04-17 23:14:18 +0200

    sd

--- src/components/contentElements/ServicesGrid.vue
+++ src/components/contentElements/ServicesGrid.vue
@@ -7,7 +7,7 @@
 			v-for="item in offer"
 			:key="item.node.id"
 		>
-			<g-image :src="item.node.backgroundimage" />
+			<g-image class="backdrop" :src="item.node.backgroundimage" />
 			<g-image class="icon mb-8" :src="item.node.icon" />
 			<h2>{{item.node.offer}}</h2>
 			<p v-html="item.node.bodytext"></p>
@@ -34,11 +34,37 @@ export default {
 		color: white;
 	}
 	bottom: 0;
+	position: relative;
+	overflow: hidden;
+	background-color: theme("colors.dark");
+	h2,
+	p,
+	a,
+	.icon {
+		position: relative;
+		z-index: 2;
+	}
+}
+.backdrop {
+	position: absolute;
+	z-index: 0;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0px;
+	height: 100%;
+	min-width: 100%;
+	opacity: 0;
+	transition: opacity 0.6s, transform 17s;
 }
 .card:hover {
-	background-color: theme("colors.secondary");
+	//background-color: theme("colors.secondary");
 	box-shadow: 0 20px 40px -12px #828282;
 	transform: translateY(-10px);
+	.backdrop {
+		opacity: 0.3;
+		transform: scale(1.25);
+	}
 }
 
 .icon {
