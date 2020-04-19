e1c1017580120f0d12ef13ad60f694484f7e46a1
Markus Steiner <office@stagedive.media>
2020-04-19 13:47:17 +0200

    sd

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,6 +6,7 @@
 		</div>
 		<sd-referenzen-slider :referenz="$page.referenz.edges"></sd-referenzen-slider>
 		<sd-about-me :about="$page.about.edges"></sd-about-me>
+            <sd-footer></sd-footer>
 
 		<br />
 		<br />
